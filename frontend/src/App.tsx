ffe8942fb0f1374a60ab994ac7cc8f12ad140cfe
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-03-21 19:33:20 +0100

    Added Typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import css from './App.scss';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className={css('dupa')}
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
