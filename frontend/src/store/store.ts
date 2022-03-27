b115c2c60824d2a30f0c97445357e800f9fa9262
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-03-27 22:25:13 +0200

    TC-9

---
+++ frontend/src/store/store.ts
@@ -0,0 +1,13 @@
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import mainReducer from './main.reducer';
+import mainSaga from './main.saga';
+
+interface RootState {}
+
+const initialState: RootState = {};
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(mainReducer, initialState, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(mainSaga);
+
+export default store;
