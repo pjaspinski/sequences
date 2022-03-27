48b3157c6d9174b9bf7cf0499560d5fcdfb5ca11
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-03-27 22:43:12 +0200

    TC-17

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,13 +1,17 @@
-import { applyMiddleware, createStore } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import mainReducer from './main.reducer';
-import mainSaga from './main.saga';
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import mainReducer from "./main.reducer";
+import mainSaga from "./main.saga";
 
 interface RootState {}
 
 const initialState: RootState = {};
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(mainReducer, initialState, applyMiddleware(sagaMiddleware));
+const store = createStore(
+    mainReducer,
+    initialState,
+    applyMiddleware(sagaMiddleware)
+);
 sagaMiddleware.run(mainSaga);
 
 export default store;
