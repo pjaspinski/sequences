16845c992f22b838f97837a7df2a17ec03227e59
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-04-09 22:51:03 +0200

    TC-10, replaced PM2 with Nodemon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('dupa')
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    console.log("dupa4");
 }
 
 bootstrap();
