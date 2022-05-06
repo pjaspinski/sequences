018ee473292d53db670042b046bf61fcc4785723
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-05-06 23:19:17 +0200

    TC-42

--- backend/src/sequences-storage/sequencesStorage.ts
+++ backend/src/sequences-storage/sequencesStorage.ts
@@ -1,8 +1,9 @@
 import { join, dirname } from "path";
 import { Low, JSONFile } from "lowdb";
 import fp from "fastify-plugin";
-import { existsSync, mkdirSync, readdirSync } from "node:fs";
-import { Sequence } from "./interfaces.js";
+import { existsSync, mkdirSync, readdirSync, unlinkSync } from "node:fs";
+import { Sequence, SequencesStorage } from "./interfaces.js";
+import initialContent from "./initialContent.js";
 
 const STORAGE_PATH = "data/sequences";
 
@@ -12,14 +13,14 @@ const loadExistingSequences = async (logger): Promise<Low<Sequence>[]> => {
     if (existsSync(storageDir)) {
         const files = readdirSync(storageDir);
 
-        files.reduce(async (dbs, filename: string) => {
+        return files.reduce(async (dbs, filename: string) => {
             const fullPath = join(dirname("."), STORAGE_PATH, filename);
-            let db: Low | undefined;
+            let db: Low<Sequence> | undefined;
             try {
                 db = new Low(new JSONFile<Sequence>(fullPath));
                 await db.read();
             } catch {
-                logger.warn(`Failed to load sequence located in ${filename}.`);
+                logger.error(`Failed to load sequence located in ${filename}.`);
                 return dbs;
             }
 
@@ -33,8 +34,63 @@ const loadExistingSequences = async (logger): Promise<Low<Sequence>[]> => {
 
 const sequencesStorage = async (fastify, options, done) => {
     const sequences = await loadExistingSequences(fastify.log);
+    const storageDir = join(dirname("."), STORAGE_PATH);
 
-    fastify.decorate("sequences", sequences);
+    const add = async (name: string) => {
+        const files = readdirSync(storageDir).map((file) => file.slice(0, -5));
+
+        if (
+            sequences.find((sequence) => sequence.data.name === name) ||
+            files.find((file) => file === name)
+        ) {
+            fastify.log.error(
+                `Failed to create new sequence with name ${name}. Sequence with this name already exists.`
+            );
+            throw new Error("Sequence with this name already exists.");
+        }
+
+        const fullPath = join(storageDir, `${name}.json`);
+        let db: Low<Sequence> | undefined;
+        try {
+            db = new Low(new JSONFile<Sequence>(fullPath));
+            await db.read();
+        } catch {
+            fastify.log.error(`Failed to create sequence called ${name}.`);
+            throw new Error("Failed to create sequence.");
+        }
+
+        await db.read();
+        db.data = { ...initialContent, name };
+        await db.write();
+
+        sequences.push(db);
+    };
+
+    const remove = (name: string) => {
+        const index = sequences.findIndex(
+            (sequence) => sequence.data.name === name
+        );
+        if (index === -1) {
+            fastify.log.error(`Failed to delete sequence with name ${name}.`);
+            throw new Error("Failed to delete sequence.");
+        }
+        sequences.splice(index, 1)[0];
+        const fullPath = join(storageDir, `${name}.json`);
+        try {
+            unlinkSync(fullPath);
+        } catch (err) {
+            fastify.log.error(`Failed to delete file: ${fullPath}.`);
+            throw new Error("Failed to delete sequence file.");
+        }
+    };
+
+    const sequencesStorage: SequencesStorage = {
+        sequences,
+        add,
+        remove,
+    };
+
+    fastify.decorate("sequences", sequencesStorage);
 
     done();
 };
