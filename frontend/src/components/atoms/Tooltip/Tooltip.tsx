ced90bb8fa47f4ae8ff6747da68516cd24835a8a
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-09-14 23:48:59 +0200

    TC-86 (#30)

--- frontend/src/components/atoms/Tooltip/Tooltip.tsx
+++ frontend/src/components/atoms/Tooltip/Tooltip.tsx
@@ -4,11 +4,20 @@ import { Popup } from "semantic-ui-react";
 interface Props {
     content: string;
     trigger: ReactNode;
+    disabled?: boolean;
 }
 
 const Tooltip = (props: Props) => {
-    const { content, trigger } = props;
-    return <Popup inverted content={content} position="top center" trigger={trigger} />;
+    const { content, trigger, disabled } = props;
+    return (
+        <Popup
+            inverted
+            disabled={disabled}
+            content={content}
+            position="top center"
+            trigger={trigger}
+        />
+    );
 };
 
 export default Tooltip;
